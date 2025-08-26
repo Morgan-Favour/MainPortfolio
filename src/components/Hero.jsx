013c6f7ea38a2fa05b6067ae2e405007267780ef
Morgan <morganfavour85@gmail.com>
2025-08-26 07:59:11 +0100

    Restore ComputersCanvas component in Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,9 +22,10 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <div className="relative w-full h-[60vh] sm:h-full">
-        {/* <ComputersCanvas /> */}
+      <div className="relative w-full h-[60vh] sm:h-full hidden md:block">
+        <ComputersCanvas />
       </div>
+
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
